cbcf0780f302e6a6a8a8c9ab2ad2a11f27d9cb8d
Steven Bendit <steven.bendit@tradeweb.com>
2022-02-10 13:54:34 -0500

    Completed module

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -2,7 +2,6 @@ const connection = new WebSocket("ws://localhost:3000");
 const logWindow = document.querySelector("#log-window");
 const filePath = document.getElementById("logFilePath").value;
 connection.onopen = () => {
-  // connection.send("Hello from the client!");
   connection.send(filePath);
 };
 connection.onmessage = (event) => {
